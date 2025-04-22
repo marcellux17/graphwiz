f170a2ac2ff3ae2fe8f8468d16a0d0298ef77c5d
Pete Marcell <pete.marcell.csigusz@gmail.com>
2025-04-22 17:46:34 +0200

    created mapping between visjs graph and my own internal

---
+++ src/datastructures/graphs.ts
@@ -0,0 +1,84 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class undirectedGraph{
+    private Nodes: Map<string, Node>;
+    private Labels: Map<string, boolean>;
+    private i:number = 1;//for labels 
+    constructor(){
+        this.Nodes = new Map<string, Node>();
+        this.Labels = new Map<string, boolean>();
+    }
+    AddNode():{id: string, label: string}{
+        const id = uuidv4();
+        const label = `${this.i}`
+        const newNode = new Node(label, id);
+        this.Labels.set(label, true);
+        this.Nodes.set(id, newNode);
+        this.i++;
+        return {id, label};
+    }
+    //for debugging purposes
+    PrintGraph():void{
+        for(const [nodeId, node] of this.Nodes){
+            console.log(`Node: ${node.label}`)
+            for(const [nId, is_neighbour] of node.AdjacencyList){
+                console.log(`Neighbour: ${this.Nodes.get(nId)?.label}`)
+            }
+            console.log('---------------------')
+        }
+    }
+    // LabelExist(label: string):boolean{
+    //     const l = this.Labels.get(label);
+    //     return l || false;
+    // }
+    ModifyLabel(id:string, new_label:string){
+        const node = this.Nodes.get(id)!;
+        let prevLabel = node.label;
+        this.Labels.delete(prevLabel);
+        node.label = new_label;
+        this.Labels.set(new_label, true);
+    }
+    AddEdge(from: string, to: string){
+        const firstNode = this.Nodes.get(from)!;
+        const secondNode = this.Nodes.get(to)!;
+        firstNode.AddNeighbour(to);
+        secondNode.AddNeighbour(from);
+    }
+    RemoveEdge(from: string, to: string){
+        console.log(`removed edge  (${from}, ${to})`)
+        const firstNode = this.Nodes.get(from)!;
+        const secondNode = this.Nodes.get(to)!;
+        firstNode.RemoveNeighbour(to);
+        secondNode.RemoveNeighbour(from);
+    }
+    DeleteNode(id:string):void{
+        const neighbours = this.Nodes.get(id)!.AdjacencyList;
+         this.Nodes.delete(id);
+         for(const [neighbourId, isNeighbour] of neighbours){
+            const node = this.Nodes.get(neighbourId)!;
+            node.RemoveNeighbour(id);
+         }
+    }
+}
+class Node{
+    label:string;
+    id:string;
+    AdjacencyList: Map<string, boolean>;
+    
+    constructor(label:string, id:string){
+        this.label = label;
+        this.id = id;
+        this.AdjacencyList = new Map<string, boolean>;
+    }
+    
+    HasNeighbour(id:string):boolean{
+        const isNeighbour = this.AdjacencyList.get(id);
+        return isNeighbour || false;
+    }
+    RemoveNeighbour(id:string):void{
+        this.AdjacencyList.delete(id);
+    }
+    AddNeighbour(id:string):void{
+        this.AdjacencyList.set(id, true);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,38 @@
 import { Node, Edge } from "vis-network";
 import { DataSet, Network } from "vis-network/standalone";
+import { undirectedGraph } from "./datastructures/graphs";
 
 const container = document.querySelector<HTMLElement>("#container")!;
 const addEdgeButton = document.querySelector<HTMLButtonElement>("#add-edge");
 const focusButton = document.querySelector<HTMLButtonElement>("#focus-view");
 const addNodeButton = document.querySelector<HTMLButtonElement>("#add-node");
-const deleteModeButton = document.querySelector<HTMLButtonElement>("#delete-mode");
-const escapeModeButton = document.querySelector<HTMLButtonElement>("#escape-mode");
-let messageBox = document.querySelector<HTMLDivElement>("#message-box")!;
-let canvas: HTMLCanvasElement;
+const deleteModeButton =
+    document.querySelector<HTMLButtonElement>("#delete-mode");
+const escapeModeButton =
+    document.querySelector<HTMLButtonElement>("#escape-mode");
+const messageBox = document.querySelector<HTMLDivElement>("#message-box")!;
 
+const label = document.querySelector<HTMLLabelElement>("#label-message")!;
+const labelInput = document.querySelector<HTMLInputElement>("#change-label")!;
+const inputGroup = document.querySelector<HTMLDivElement>("#change-data")!;
+//const messageForLabel = document.querySelector<HTMLParagraphElement>("#change-data .message-box")!;
+const visualizeGraph =
+    document.querySelector<HTMLButtonElement>("#visualize-graph");
+visualizeGraph?.addEventListener("click", () => {
+    graph.PrintGraph();
+});
 //types
-type editMode = "add-edge-mode" | "none" | "delete" | "add-node-mode"
+type editMode = "add-edge-mode" | "none" | "delete" | "add-node-mode";
 
-//GLOBAL FLAGS
+//GLOBAL VARIABLES
+let selectedNode: string;
 let edit_mode: editMode = "none";
 
-const nodes = new DataSet<Node>([
-    { id: 1, label: "N 1" },
-    { id: 2, label: "N 2" },
-    { id: 3, label: "N 3" },
-    { id: 4, label: "N 4" },
-]);
-
-const edges = new DataSet<Edge>([
-    { from: 1, to: 3, id: 123 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 4 },
-]);
+const graph_nodes = new DataSet<Node>([]);
+const graph_edges = new DataSet<Edge>([]);
 const data = {
-    nodes: nodes,
-    edges: edges,
+    nodes: graph_nodes,
+    edges: graph_edges,
 };
 const options = {
     width: "700px",
@@ -58,79 +59,118 @@ const options = {
     manipulation: {
         enabled: false,
         addEdge: function (edgeData: Edge, callback: Function) {
-            if (edgeData.from !== edgeData.to) {
+            const from = edgeData.from;
+            const to = edgeData.to!;
+            if (from !== to) {
+                if (typeof from === "string" && typeof to == "string") {
+                    graph.AddEdge(from, to);
+                }
                 callback(edgeData);
             }
-            changeMode('add-edge-mode')
+            changeMode("add-edge-mode");
         },
         addNode: function (nodeData: Node, callback: Function) {
+            const { id, label } = graph.AddNode();
+            nodeData.id = id;
+            nodeData.label = label;
             callback(nodeData);
-            changeMode('add-node-mode')
+            changeMode("add-node-mode");
+        },
+        deleteNode: function (
+            { edges, nodes }: { edges: string[]; nodes: string[] },
+            callback: Function
+        ) {
+            const id = nodes[0];
+            if (typeof id === "string") {
+                graph.DeleteNode(id);
+            }
+            const data = { edges, nodes };
+            callback(data);
+            resetInput();
+        },
+        deleteEdge: function (
+            { edges, nodes }: { edges: string[]; nodes: string[] },
+            callback: Function
+        ) {
+            const edge_to_be_deleted: Edge = graph_edges.get(edges[0])!;
+            const from = edge_to_be_deleted.to;
+            const to = edge_to_be_deleted.from;
+            if (typeof from === "string" && typeof to == "string") {
+                graph.RemoveEdge(from, to);
+            }
+            const data = { edges, nodes };
+            callback(data);
         },
     },
 };
-
 const network = new Network(container, data, options);
-
-let selectNode: number | undefined;
+const graph = new undirectedGraph();
 //events on the network
 network.on("selectNode", (e) => {
-    selectNode = e.nodes[0];
+    if (edit_mode !== "none") {
+        return;
+    }
+    selectedNode = e.graph_nodes[0];
+    const node = graph_nodes.get(selectedNode);
+    inputGroup.classList.remove("hide");
+    label.textContent = `Change label of the selected node`;
+    labelInput.value = node?.label == undefined ? "" : node.label;
 });
-
-edges.add({
-    from: 1,
-    to: 10,
+network.on("deselectNode", () => {
+    resetInput();
 });
 addEdgeButton?.addEventListener("click", () => {
-    changeMode('add-edge-mode')
+    changeMode("add-edge-mode");
 });
 addNodeButton?.addEventListener("click", () => {
-    changeMode('add-node-mode')
+    changeMode("add-node-mode");
 });
 focusButton?.addEventListener("click", () => {
     network.fit();
 });
 deleteModeButton?.addEventListener("click", () => {
-    changeMode('delete')
+    changeMode("delete");
 });
 escapeModeButton?.addEventListener("click", () => {
-  changeMode('none')
-})
+    changeMode("none");
+});
+labelInput.addEventListener("input", () => {
+    const new_label = labelInput.value;
+    graph.ModifyLabel(selectedNode, new_label);
+    graph_nodes.update({
+        id: selectedNode,
+        label: new_label,
+    });
+});
 
-//mutation observer for displaying editing modes and messages on canvas
-const observer = new MutationObserver((mutations, obs) => {
-    const c = container.querySelector("canvas");
-    if (c) {
-        canvas = c;
-        obs.disconnect();
+function changeMode(mode: editMode) {
+    edit_mode = mode;
+    let additionalmessage;
+    switch (mode) {
+        case "add-edge-mode":
+            additionalmessage =
+                "to create an edge click and drag from one node to the other";
+            network.addEdgeMode();
+            break;
+        case "add-node-mode":
+            additionalmessage = "click on a blank position to add node";
+            network.addNodeMode();
+            break;
+        case "delete":
+            additionalmessage = "click on an edge or a node to delete it";
+            network.deleteSelected();
+            break;
+        case "none":
+            additionalmessage = `select mode on the toolbar`;
+            network.disableEditMode();
+            break;
     }
-});
-observer.observe(container, {
-    childList: true,
-    subtree: true,
-});
-
-function changeMode(mode:editMode){
-  edit_mode = mode
-  let additionalmessage
-  switch(mode){
-    case 'add-edge-mode':
-      additionalmessage = 'to create an edge click and drag from one node to the other'
-      network.addEdgeMode();
-      break;
-    case 'add-node-mode':
-      additionalmessage = 'click on a blank position to add node'
-      network.addNodeMode();
-      break;
-    case 'delete':
-      additionalmessage = 'click on an edge or a node to delete it'
-      network.deleteSelected();
-      break;
-    case 'none':
-      additionalmessage = `messages will appear here`
-      network.disableEditMode();
-      break;
-  }
-  messageBox.textContent = additionalmessage;
+    messageBox.textContent = additionalmessage;
+}
+function resetInput() {
+    selectedNode = "";
+    label.textContent = "";
+    labelInput.value = "";
+    inputGroup.classList.add("hide");
+    //   messageForLabel.classList.add('hidden')
 }
